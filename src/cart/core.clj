e1d84a9f7dcc4e72197636523c221a36bc9f70ff
Rhys Roberts <rhys@mega-city-one.co.uk>
2014-10-15 22:11:34 +0100

    add pre-condition validations

--- src/cart/core.clj
+++ src/cart/core.clj
@@ -7,33 +7,10 @@
   (println "Hello, World!"))
 
 
-
-(defn create-cart []
-  {:items {}}
-  )
-
-
-(defn cart-line-count [cart]
-  (count (cart :items)))
-
-
-(defn cart-item-add [cart item]
-  (let [product-id (item :product-id)]
-    (update-in cart [:items]
-      (fn [items]
-        (if (contains? items product-id)
-          (update-in items [product-id]
-            (fn [current-item]
-              (update-in current-item [:quantity]
-                (fn [quantity] (+ quantity (item :quantity))))))
-          (assoc items product-id item))))))
-
-
 (defn valid-cart? [cart]
   (if (not (contains? cart :items))
     false
     true))
-
 
 (defn valid-product-id? [product-id]
   (and (integer? product-id) (> product-id 0)))
@@ -49,3 +26,28 @@
     (if (not (and (contains? line-item :quantity) (valid-quantity? (line-item :quantity))))
       false
       true)))
+
+(defn create-cart []
+  {:items {}}
+  )
+
+
+(defn cart-line-count [cart]
+  {:pre [(valid-cart? cart)]}
+  (count (cart :items)))
+
+
+(defn cart-item-add [cart item]
+  {:pre [(valid-cart? cart)
+         (valid-line-item? item)]}
+  (let [product-id (item :product-id)]
+    (update-in cart [:items]
+      (fn [items]
+        (if (contains? items product-id)
+          (update-in items [product-id]
+            (fn [current-item]
+              (update-in current-item [:quantity]
+                (fn [quantity] (+ quantity (item :quantity))))))
+          (assoc items product-id item))))))
+
+
